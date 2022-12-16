5b2f754bc36bf105740c72ad62c43a8a201cbfd8
湫 <2771204970@qq.com>
2022-12-16 13:55:27 +0800

    i

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 print("Hello qiuqiu")
 i = print("nihao")
 print("i")
+dir(__builtins__)
 
+i = 456
+print(i)
+
+x = 2
+y = 5
+print(x,y)
+y = x
+print(y)
+x = 2
+y = 5
+x,y=y,x
+print(x,y)
+
+print('go')
+print("let's go")
+print("\"let\'s go\"")
+print("i love python \ni love pychram")
 
 
